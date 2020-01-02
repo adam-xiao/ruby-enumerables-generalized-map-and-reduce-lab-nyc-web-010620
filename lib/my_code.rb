768e0acc0eca2d8d93af59737365b0bdbec7d809
Adam Xiao <adam.x.xiao@gmail.com>
2020-01-02 22:55:35 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,26 @@
-# Your Code Here
+def map(source_array)
+  x = 0
+  new_array = []
+
+  while x < source_array.length do
+    new_array << yield(source_array[x])
+    x += 1
+  end
+  new_array
+end
+
+def reduce(source_array, starting_point = nil)
+  x = 0
+
+  if starting_point
+    total = starting_point
+  else
+    total = 0
+  end
+
+  while x < source_array.length do
+    total = total + source_array[x]
+    x += 1
+  end
+  total
+end
